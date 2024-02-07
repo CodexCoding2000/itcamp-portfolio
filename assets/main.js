63f960b533c6c712fad68b1177ee6d58831fc74f
CodexCoding2000 <codexcoding99@gmail.com>
2024-02-07 17:58:12 +0700

    make navbar responsive

--- assets/main.js
+++ assets/main.js
@@ -6,22 +6,48 @@ scrolltotop = () => {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    const navs = document.querySelectorAll("[goto]");
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("[goto]");
+
     window.addEventListener("scroll", e => {
-        if (window.scrollY > 70) {
+        const top = window.scrollY;
+
+        if (top > 70) {
             navbar.classList.add("scroll");
-        } else if (window.scrollY <= 20) {
+        } else if (top <= 20) {
             navbar.classList.remove("scroll");
         }
     
-        if (window.scrollY > 140) {
+        if (top > 140) {
             scrollToTop.classList.add("show");
-        } else if (window.scrollY <= 70) {
+        } else if (top <= 70) {
             scrollToTop.classList.remove("show");
         }
+
+        sections.forEach(sec => {
+            const offset = sec.offsetTop - 250;
+            const height = sec.offsetHeight;
+            const page   = sec.getAttribute("id");
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove("active");
+                })
+
+                const node = document.querySelectorAll(`[goto="${page === "home" ? "top" : page}"]`);
+
+                if (node) {
+                    node.forEach(elem => elem.classList.add("active"));
+                }
+            }
+        })
     });
 
-    navs.forEach(element => {
+    document.querySelector(".navbar-toggle").addEventListener("click", e => {
+        document.querySelector(".navbar").classList.toggle("show");
+    })
+
+    navLinks.forEach(element => {
         element.addEventListener("click", e => {
             const attr = element.getAttribute("goto");
             
